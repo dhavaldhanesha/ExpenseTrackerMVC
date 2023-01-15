5869eea184b58d03b0e850fdcb3211597fe1b910
DHAVAL DHANESHA <48144575+dhavaldhanesha@users.noreply.github.com>
2023-01-15 15:58:28 +0530

    Add files via upload

---
+++ Expense Tracker MVC/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExpTrackerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Expense Tracker MVC/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ExpTrackerAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
