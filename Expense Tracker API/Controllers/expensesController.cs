05064a3e94031848d2e65220a3c7ee3e387865dd
DHAVAL DHANESHA <48144575+dhavaldhanesha@users.noreply.github.com>
2023-01-15 15:48:19 +0530

    Add files via upload

---
+++ Expense Tracker API/Controllers/expensesController.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ExpTrackerAPI.Models;
+
+namespace ExpTrackerAPI.Controllers
+{
+    public class expensesController : ApiController
+    {
+        private ExpenseTrackerEntities db = new ExpenseTrackerEntities();
+
+        // GET: api/expenses
+        /*public IQueryable<expense> Getexpenses()
+        {
+            return db.expenses;
+        }*/
+
+        public IHttpActionResult GetExpenses()
+        {
+            var expenses = db.expenses.Join(db.categories, e => e.categoryId, c => c.categoryId, (e, c) => new { e, c })
+                .Select(ec => new categoryName()
+                {
+                    expenseId = ec.e.expenseId,
+                    CategoryName = ec.c.categoryName,
+                    expenseTitle = ec.e.expenseTitle,
+                    expenseDesription = ec.e.expenseDesription,
+                    date = ec.e.date,
+                    expenseAmount = ec.e.expenseAmount
+                }).ToList();
+            return Ok(expenses);
+        }
+
+        // GET: api/expenses/5
+        [ResponseType(typeof(expense))]
+        public IHttpActionResult Getexpense(int id)
+        {
+            expense expense = db.expenses.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(expense);
+        }
+
+        // PUT: api/expenses/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putexpense(int id, expense expense)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != expense.expenseId)
+            {
+                return BadRequest();
+            }
+            expense.date = DateTime.Now;
+            if(expense.expenseTitle == "")
+            {
+                expense.expenseTitle = "None";
+            }
+            db.Entry(expense).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!expenseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/expenses
+        [ResponseType(typeof(expense))]
+        public IHttpActionResult Postexpense(expense expense)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            expense.date = DateTime.Now;
+            
+            db.expenses.Add(expense);
+            
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = expense.expenseId }, expense);
+        }
+
+        // DELETE: api/expenses/5
+        [ResponseType(typeof(expense))]
+        public IHttpActionResult Deleteexpense(int id)
+        {
+            expense expense = db.expenses.Find(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            var obj = db.categories.FirstOrDefault(x => x.categoryId == expense.categoryId);
+            obj.categoryExpense = obj.categoryExpense - expense.expenseAmount;
+            db.expenses.Remove(expense);
+            db.SaveChanges();
+
+            return Ok(expense);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool expenseExists(int id)
+        {
+            return db.expenses.Count(e => e.expenseId == id) > 0;
+        }
+
+        [System.Web.Http.Route("api/expenses/categoryNameList")]
+        public IHttpActionResult GetcategoryNameList()
+        {
+            var categoryNameList = db.categories.Select(e => e.categoryName).ToList();
+            return Ok(categoryNameList);
+        }
+
+        [Route("api/expenses/expenseAmount/{id}")]
+        public IHttpActionResult GetexpenseAmount(int id)
+        {
+            var expenseAmount = db.expenses.Where(x => x.expenseId == id).Select(e => e.expenseAmount).ToList();
+            return Ok(expenseAmount);
+        }
+        //CATEGORY AMOUNT: 
+        [Route("api/expenses/totalExpenseByCategory")]
+        public decimal PostTotalCategoryAmount(expense expense)
+        {
+            var totCatExp = db.expenses.Where(e => e.categoryId == expense.categoryId).Sum(e => e.expenseAmount);
+            return totCatExp;
+        }
+
+        //GET EXPENSE BY CATEGORY: 
+        [Route("api/expenses/ExpenseByCategory/{id}")]
+        public IHttpActionResult GetExpenseByCategory(int id)
+        {
+            var expenses = db.expenses.Join(db.categories, e => e.categoryId, c => c.categoryId, (e, c) => new { e, c })
+                .Where(ec => ec.e.categoryId == id)
+                .Select(ec => new categoryName()
+                {
+                    expenseId = ec.e.expenseId,
+                    CategoryName = ec.c.categoryName,
+                    expenseTitle = ec.e.expenseTitle,
+                    expenseDesription = ec.e.expenseDesription,
+                    date = ec.e.date,
+                    expenseAmount = ec.e.expenseAmount
+                }).ToList();
+            return Ok(expenses);
+        }
+    }
+}
---
+++ Expense Tracker API/Models/home.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExpTrackerAPI.Models
+{
+    public class home
+    {
+        public IEnumerable<category> Categories { get; set; }
+
+        public IEnumerable<categoryName> Expenses { get; set; }
+
+        public decimal Limit { get; set; }
+        public decimal ExpenseAmt { get; set; }
+
+        public int usage { get; set; }
+        public int categoryId { get; set; }
+
+        public string categoryName { get; set; }
+        public int categoryExpense { get; set; }
+
+        [Required(ErrorMessage = "Category Limit is Mendatory")]
+        public int categoryLimit { get; set; }
+
+        public int id { get; set; }
+        public int totalExpenseLimit { get; set; }
+
+        public int actualExpense { get; set; }
+
+        public int expenseId { get; set; }
+        public string expenseTitle { get; set; }
+        public string expenseDesription { get; set; }
+        public int expenseAmount { get; set; }
+        public System.DateTime date { get; set; }
+    }
+}
